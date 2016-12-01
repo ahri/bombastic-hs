24e47b6e2001f26ca5c8b59543173097571ab42a
Adam Piper <adam@ahri.net>
2016-12-01 08:24:03 +0000

    Neaten up test.

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,24 +2,23 @@ import Test.Hspec
 import Data.List
 import Bombastic
 
-validMap :: [String]
-validMap =
-    [ "######"
-    , "#S.  #"
-    , "#.S..#"
-    , "#.. S#"
-    , "######"
-    ]
-
 main :: IO ()
 main = hspec $ do
     describe "Map load" $ do
         it "valid map loads correctly" $ do
             opaque `shouldBe` expected
                 where
-                    players = [mkPlayer "p1", mkPlayer "p2"]
-                    state = startGame players <$> mapFromDebug validMap
                     opaque = show . opaqueState <$> state
+                    state = startGame players <$> mapFromDebug validMap
+
+                    players = [mkPlayer "p1", mkPlayer "p2"]
+                    validMap =
+                        [ "######"
+                        , "#S.  #"
+                        , "#.S..#"
+                        , "#.. S#"
+                        , "######"
+                        ]
                     expected = Just (intercalate "\n"
                         [ "######"
                         , "#0.  #"
